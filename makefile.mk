af14479216e396e61243e1f716e34fb177ae2e75
Manigandan <mani007430@gmail.com>
2022-02-23 09:06:27 +0530

    README.md

--- makefile.mk
+++ makefile.mk
@@ -3,7 +3,7 @@
 README.md :
 	touch README.md
 	echo "# UNIX-WORKBENCH-PROJECT">>README.md
-	echo "-THE DATE AND TIME AT WHICH make was Run: `date`">>README.md
+	echo "-THE DATE AND TIME AT WHICH make was Run: `date`">>README.md \
 	echo "-The number of lines of code contained in guessinggame.sh: `wc -l guessinggame.sh | egrep -o "[0-9]+"`">> README.md
 
 clean:
