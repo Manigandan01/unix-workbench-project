3ea46fac5ad24685c9c8541dee90db7ffd6cbdee
Manigandan <mani007430@gmail.com>
2022-02-23 09:23:30 +0530

    Added Bold rules in .md

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Guessing Game
-- THE DATE AND TIME AT WHICH make was Run: Wed Feb 23 09:18:28 IST 2022
-- The number of lines of code contained in guessinggame.sh: 23
+- THE DATE AND TIME AT WHICH make was Run: *Wed Feb 23 09:22:31 IST 2022*
+- The number of lines of code contained in guessinggame.sh: *23*
--- makefile.mk
+++ makefile.mk
@@ -3,8 +3,8 @@ build :README.md
 README.md :
 	touch README.md
 	echo "# Guessing Game">>README.md
-	echo "- THE DATE AND TIME AT WHICH make was Run: `date`">>README.md 
-	echo "- The number of lines of code contained in guessinggame.sh: `wc -l guessinggame.sh | egrep -o "[0-9]+"`">> README.md
+	echo "- THE DATE AND TIME AT WHICH make was Run: *`date`*">>README.md 
+	echo "- The number of lines of code contained in guessinggame.sh: *`wc -l guessinggame.sh | egrep -o "[0-9]+"`*">> README.md
 
 clean:
 	rm -rf README.md
