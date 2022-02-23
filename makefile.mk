188c1198b0072121daf274f5bf6a75cb1a3c8a57
Manigandan <mani007430@gmail.com>
2022-02-23 09:02:50 +0530

    made some changes for counting line's

--- makefile.mk
+++ makefile.mk
@@ -3,9 +3,8 @@
 README.md :
 	touch README.md
 	echo "# UNIX-WORKBENCH-PROJECT">>README.md
-	echo "\nTHE DATE AND TIME AT WHICH make was Run: `date`">>README.md
-	echo "\nThe number of lines of code contained in guessinggame.sh:" >> README.md
-	wc -l guessinggame.sh | egrep -o "[0-9]+" >> README.md
+	echo "-THE DATE AND TIME AT WHICH make was Run: `date`">>README.md
+	echo "-The number of lines of code contained in guessinggame.sh: `wc -l guessinggame.sh | egrep -o "[0-9]+"`">> README.md
 
 clean:
 	rm -rf README.md
